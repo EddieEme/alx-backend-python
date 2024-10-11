4ad4e4b68165fd5b917ed60de22e64c9583553af
Emeremnu Edison <edisonemeremnu@gmail.com>
2024-10-11 23:39:17 +0100

    updated task 101

--- 0x00-python_variable_annotations/101-safely_get_value.py
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -15,6 +15,7 @@ T = typing.TypeVar('T')
 def safely_get_value(dct: typing.Mapping, key: typing.Any, default:
                      typing.Union[T, None] = None) ->\
                         typing.Union[typing.Any, T]:
+    """Return parameters and type annotations"""
     if key in dct:
         return dct[key]
     else:
